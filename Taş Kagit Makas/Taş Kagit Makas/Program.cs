bf120eda37349b87e65b0bb0cea0a3e25b65db9c
SibopADAM <burakefekideys@gmail.com>
2025-11-17 10:24:58 +0300

    Add files via upload

---
+++ Taş Kagit Makas/Taş Kagit Makas/Program.cs
@@ -0,0 +1,93 @@
+﻿int bizimskor = 0;
+int rakipskor = 0;
+string hamle;
+int robot;
+
+bool oyunbitti = false;
+Random sayi = new Random();
+
+Console.WriteLine("TAŞ KAGIT OYUNUNA HOŞGELDİNİZ 8)");
+
+while (oyunbitti == false)
+{
+    Console.Write("Hamlenizi Yapin: ");
+    hamle = Console.ReadLine();
+    robot = sayi.Next(1, 3);
+    switch (hamle)
+    {
+        case "taş":
+            switch (robot)
+            {
+                case 1:
+                    Console.WriteLine("Taş");
+                    Console.WriteLine("Berabere");
+                    break;
+                case 2:
+                    Console.WriteLine("Kagit");
+                    Console.WriteLine("Rakip 1 Puan Kazandi");
+                    rakipskor++;
+                    break;
+                case 3:
+                    Console.WriteLine("Maakas");
+                    Console.WriteLine("1 Puan Kazandin");
+                    bizimskor++;
+                    break;
+            }
+            break;
+        case "kagit":
+            switch (robot)
+            {
+                case 1:
+                    Console.WriteLine("Taş");
+                    Console.WriteLine("1 Puan Kazandin");
+                    bizimskor++;
+                    break;
+                case 2:
+                    Console.WriteLine("Kagit");
+                    Console.WriteLine("Berabere");
+                    break;
+                case 3:
+                    Console.WriteLine("Makas");
+                    Console.WriteLine("Rakip 1 Puan Kazandi");
+                    rakipskor++;
+                    break;
+            }
+            break;
+        case "makas":
+            switch (robot)
+            {
+                case 1:
+                    Console.WriteLine("Taş");
+                    Console.WriteLine("Rakip 1 Puan Kazandi");
+                    rakipskor++;
+                    break;
+                case 2:
+                    Console.WriteLine("Kagit");
+                    Console.WriteLine("1 Puan Kazandin");
+                    bizimskor++;
+                    break;
+                case 3:
+                    Console.WriteLine("Makas");
+                    Console.WriteLine("Berabere");
+                    break;
+            }
+            break;
+
+    }
+
+    if (bizimskor == 3)
+    {
+        Console.WriteLine("TEBRİKLER KAZANDINIZ 8)");
+        oyunbitti = true;
+    }
+    else if (rakipskor == 3)
+    {
+        Console.WriteLine("ÜZGÜNÜM MALESEF KAYBETTİN 8(");
+        oyunbitti = true;
+    }
+    Console.WriteLine(bizimskor + " / " + rakipskor);
+    
+}
+
+
+
---
+++ vücut kitle endeksi hesaplayıcı/vücut kitle endeksi hesaplayıcı/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Write("Kilonuzu Yaziniz: ");
+int kg = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Boyunuzu cm Cinsinden Yaziniz: ");
+int boy = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Vücut Kitle Endeksiniz Şu Şekildedir: ");
+double endeks = (double)kg / (((double)boy / 100) * ((double)boy / 100));
+Console.Write(endeks);
+
+if (endeks < 18.5)
+{
+    Console.WriteLine(" Kilonuz: Cok Zayif");
+}
+else if (endeks >= 18.5 && endeks < 25)
+{
+    Console.WriteLine(" Kilonuz: Normal");
+}
+else if (endeks >= 25 && endeks < 30)
+{
+    Console.WriteLine(" Kilonuz: Fazla Kilolu");
+}
+else if (endeks >= 30 && endeks < 40)
+{
+    Console.WriteLine(" Kilonuz: Obez");
+}
+else if (endeks >= 40)
+{
+    Console.WriteLine(" Kilonuz: Aşiri Obez");
+} 
